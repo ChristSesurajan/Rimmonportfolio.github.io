0b74c5c6623605f6fe0f0b3e65be3913789b1fa7
noobwebhost <162313416+noobwebhost@users.noreply.github.com>
2024-04-09 20:39:10 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
-let j=document.getElementById("hidden1");
+let j = document.getElementById("ho1");
+let i = document.getElementById("js1");
 
-j.onclick=function(event){
-     event.style.color='red';
+j.onmouseout = function() {
+    // To change the color of the element that triggered the event (in this case, 'j')
+    i.style.display = 'none';
+}
+
+j.onmouseover = function() {
+    // To change the color of the element that triggered the event (in this case, 'j')
+    i.style.display = 'inline';
+}
+
+
+
+
+let a = document.getElementById("ho2");
+let b = document.getElementById("js2");
+
+a.onmouseout = function() {
+    // To change the color of the element that triggered the event (in this case, 'j')
+    b.style.display = 'none';
+}
+
+a.onmouseover = function() {
+    // To change the color of the element that triggered the event (in this case, 'j')
+    b.style.display = 'inline';
 }
